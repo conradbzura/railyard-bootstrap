997b402b14424c378bdf5030ad16f66c095485ff
Conrad Bzura <conradbzura@gmail.com>
2021-09-10 12:07:26 -0400

    [#20] Revert `setuptools/distutils.setup` wrapper

--- railyard/bootstrap/version/__init__.py
+++ railyard/bootstrap/version/__init__.py
@@ -1,12 +1,7 @@
 from __future__ import annotations
 
 import importlib.metadata
-import functools
-import inspect
-import sys
-from typing import Any
 
-from railyard.bootstrap.version._setup import _setup
 from railyard.bootstrap.version._version import Version
 
 
@@ -17,29 +12,3 @@ for plugin in importlib.metadata.entry_points()[
     "railyard.bootstrap.version.plugins"
 ]:
     plugin.load()
-else:
-    import railyard.bootstrap.version._git  # noqa: F401
-
-
-if "setuptools" in sys.modules:
-    import setuptools
-
-    frame = inspect.currentframe()
-    module: Any = None
-    while (
-        frame
-        and id(getattr(module, "setuptools", None)) != id(setuptools)
-        and id(getattr(module, "setup", None)) != id(setuptools.setup)
-    ):
-        frame = frame.f_back
-        module = inspect.getmodule(frame)
-    if hasattr(module, "setuptools") or hasattr(module, "setuptools.setup"):
-        module.setuptools.setup = functools.partial(
-            _setup, module.setuptools.setup
-        )
-    elif hasattr(module, "setup"):
-        module.setup = functools.partial(_setup, module.setup)
-else:
-    import setuptools
-
-    setuptools.setup = functools.partial(_setup, setuptools.setup)
